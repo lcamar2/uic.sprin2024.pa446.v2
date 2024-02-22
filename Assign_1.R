33ce0094ae1d8913150f4bad8720930a9728a3de
Lucky Camargo <lcamar2@uic.edu>
2024-02-22 17:38:33 -0600

    Checked the name of default branch after creating the Assign_1 R script file by going to terminal pane and typing git branch. The response from command was "main"-denoting the branch.

---
+++ Assign_1.R
@@ -0,0 +1,4 @@
+# Checked the name of default branch after creating 
+# the Assign_1 R script file by going to terminal 
+# pane and typing git branch. The response from 
+# command was "main"-denoting the branch.
