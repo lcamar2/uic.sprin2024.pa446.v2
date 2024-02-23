8b523de6c539bb0df4d0374136f1b0d31fd067a1
Lucky Camargo <lcamar2@uic.edu>
2024-02-23 10:39:40 -0600

    answered qs 7 on main branch via local

--- Assign_1.R
+++ Assign_1.R
@@ -2,3 +2,14 @@
 # the Assign_1 R script file by going to terminal 
 # pane and typing git branch. The response from 
 # command was "main"-denoting the branch.
+
+#7a. Which countries had the largest delegation of athletes in 1992? 
+
+delegation92=
+  Olympics %>%
+  filter(year == 1992) %>%
+  group_by(country) %>%
+  summarize(total_athletes = sum(athletes, na.rm = TRUE)) %>%
+  arrange(desc(total_athletes)) %>%
+  select(country, total_athletes)
+delegation92
