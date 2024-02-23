a8d3b32fe86a7bb6bdb5a9faea92dc8a8436dcff
Lucky Camargo <lcamar2@uic.edu>
2024-02-23 10:24:33 -0600

    load packages

--- Assign_1.R
+++ Assign_1.R
@@ -2,3 +2,22 @@
 # the Assign_1 R script file by going to terminal 
 # pane and typing git branch. The response from 
 # command was "main"-denoting the branch.
+
+
+
+#Load Packages
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+
+#Load data
+Olympics <- read_csv("Olympics.csv")
+
+
+#6a Calculate new variable, called '‘total.medals’, which is the sum of gold, silver, and
+#bronze, and add it to the Olympic dataset.
+
+Olympics=
+  Olympics %>%
+  mutate(total.medals = gold + silver + bronze)
